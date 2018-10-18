05e97c61a52c23b3b1a5b2caf48328f59ef7ccaa
Krzysztof Szumny <noisy.pl@gmail.com>
2018-10-18 23:51:39 +0200

    support for account create

--- claimed_accounts.py
+++ claimed_accounts.py
@@ -8,11 +8,22 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
+account_creators = db.collection("account_creators")
+ops = db.collection("ops")
 claim_account = db.collection("claim_account")
-claimed_accounts_per_user = db.collection("claimed_accounts_per_user")
 create_claimed_account = db.collection("create_claimed_account")
+account_create = db.collection("account_create")
 
 blockchain = Blockchain()
+
+default_fields = {
+    "claimed_accounts": 0,
+    "pending_claimed_accounts": 0,
+    "created_paid_accounts": 0,
+    "created_discounted_accounts": 0,
+    "created_accounts": 0,
+}
+
 for op in blockchain.stream(
     opNames=[
         'claim_account',
@@ -26,26 +37,19 @@ for op in blockchain.stream(
 
     print(op)
     op["timestamp"] = op["timestamp"].__str__()
+    creator = op["creator"]
+    trx_id = op["trx_id"]
 
     if op["type"] == "claim_account":
-        creator = op["creator"]
-        trx_id = op["trx_id"]
-
         claim_account.document(trx_id).set(op)
+        ops.document(trx_id).set(op)
 
-
-        creator_doc_ref = claimed_accounts_per_user.document(creator)
+        creator_doc_ref = account_creators.document(creator)
         creator_doc_ref.collection('claims').document(trx_id).set(op)
 
         fields = creator_doc_ref.get().to_dict()
         if not fields:
-            fields = {
-                "claimed_accounts": 0,
-                "pending_claimed_accounts": 0,
-                "created_paid_accounts": 0,
-                "created_discounted_accounts": 0,
-                "created_accounts": 0,
-            }
+            fields = default_fields.copy()
             creator_doc_ref.set(fields)
 
         fields["claimed_accounts"] += 1
@@ -53,10 +57,6 @@ for op in blockchain.stream(
         creator_doc_ref.update(fields)
 
     elif op['type'] == 'create_claimed_account':
-        print('create_claimed_account')
-
-        creator = op["creator"]
-        trx_id = op["trx_id"]
         new_account_name = op["new_account_name"]
 
         del op['owner']
@@ -64,19 +64,14 @@ for op in blockchain.stream(
         del op['posting']
 
         create_claimed_account.document(trx_id).set(op)
+        ops.document(trx_id).set(op)
 
-        creator_doc_ref = claimed_accounts_per_user.document(creator)
+        creator_doc_ref = account_creators.document(creator)
         creator_doc_ref.collection('created_discounted_accounts').document(new_account_name).set(op)
 
         fields = creator_doc_ref.get().to_dict()
         if not fields:
-            fields = {
-                "claimed_accounts": 0,
-                "pending_claimed_accounts": 0,
-                "created_paid_accounts": 0,
-                "created_discounted_accounts": 0,
-                "created_accounts": 0,
-            }
+            fields = default_fields.copy()
             creator_doc_ref.set(fields)
 
         fields["pending_claimed_accounts"] -= 1
@@ -86,4 +81,24 @@ for op in blockchain.stream(
         creator_doc_ref.update(fields)
 
     elif op['type'] == 'account_create':
-        print('account_create')
+        new_account_name = op["new_account_name"]
+
+        del op['owner']
+        del op['active']
+        del op['posting']
+
+        account_create.document(trx_id).set(op)
+        ops.document(trx_id).set(op)
+
+        creator_doc_ref = account_creators.document(creator)
+        creator_doc_ref.collection('created_paid_accounts').document(new_account_name).set(op)
+
+        fields = creator_doc_ref.get().to_dict()
+        if not fields:
+            fields = default_fields.copy()
+            creator_doc_ref.set(fields)
+
+        fields["created_paid_accounts"] += 1
+        fields["created_accounts"] += 1
+
+        creator_doc_ref.update(fields)
