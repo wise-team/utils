7c3eed3fc478c90e5bce5b0e5c53925fa4b5acd6
Krzysztof Szumny <noisy.pl@gmail.com>
2018-10-21 02:11:39 +0200

    dockerize everything❗

--- created-accounts/backend/created_accounts.py
+++ created-accounts/backend/created_accounts.py
@@ -2,11 +2,12 @@ from beem.blockchain import Blockchain
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+print("backend start...")
 
 cred = credentials.Certificate("./serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-
+print("Firestore connection initialized...")
 
 account_creators = db.collection("account_creators")
 ops = db.collection("ops")
@@ -100,20 +101,19 @@ pref = global_preferences.get().to_dict()
 last_block_num_synced = pref['last_block_num_synced'] if pref else 26256743 - 1  # HF20 -1
 last_block_num = last_block_num_synced + 1
 
+print("Let's sync with blockchain...")
 for op in blockchain.stream(
-    opNames=[
-        'claim_account',
-        'create_claimed_account',
-        'account_create',
-    ],
+    # opNames=[
+    #     'claim_account',
+    #     'create_claimed_account',
+    #     'account_create',
+    # ],
     # start=26256743, # first block of HF20
     # start=26265398, # first block after HF20 with `create_claimed_account` op
     # start=26289186, # first block after HF20 with `account_create` op
     start=last_block_num_synced + 1,
     max_batch_size=50,
 ):
-
-    print(op)
     op["timestamp"] = op["timestamp"].__str__()
 
     if last_block_num < op['block_num']:  # that means that all ops from last_block are already synced
@@ -128,15 +128,19 @@ for op in blockchain.stream(
         })
 
         transaction.commit()
+        print("Block #{} synced, {} blocks behind...".format(op['block_num'], current_block_num - op['block_num']))
         transaction = db.transaction()
 
     last_block_num = op['block_num']
 
     if op["type"] == "claim_account":
+        print(op)
         new_claim_account(transaction, op)
 
     elif op['type'] == 'create_claimed_account':
+        print(op)
         new_create_claimed_account(transaction, op)
 
     elif op['type'] == 'account_create':
+        print(op)
         new_account_create(transaction, op)
